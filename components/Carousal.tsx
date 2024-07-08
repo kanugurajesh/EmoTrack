f8a1a0474af1ace47a26b5997631b561278aa7c6
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-08 18:17:41 +0530

    Adding the image

--- components/Carousal.tsx
+++ components/Carousal.tsx
@@ -18,7 +18,7 @@ export function CarouselComponent() {
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                  <img src="/mentalhealth1.jpg" alt="mental health" className="w-full h-full" />
                 </CardContent>
               </Card>
             </div>

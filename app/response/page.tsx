8ef70335119ed44742c9d949eeee1a2b5c8a4121
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-11 20:13:54 +0530

    publishing the working code

--- app/response/page.tsx
+++ app/response/page.tsx
@@ -2,20 +2,15 @@
 
 import { useSelector } from 'react-redux'
 import { RootState } from '@/lib/store'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import sendGemini from '@/lib/sendGemini'
 import Markdown from 'react-markdown'
 
-export const Response = () => {
+const Response = () => {
   const score = useSelector((state: RootState) => state.score.value)
   const [response, setResponse] = useState<string>('')
 
-  const prompt =
-    'I got a score of ' +
-    score +
-    ' on the PHQ-9.' +
-    ' Can you tell me about the severity of my depression?'
+  const prompt = `I got a score of ${score} on the PHQ-9. Can you tell me about the severity of my depression?`
   const tokens = 1000
 
   useEffect(() => {
@@ -24,7 +19,7 @@ export const Response = () => {
       setResponse(response)
     }
     fetchData()
-  }, [])
+  }, [prompt, tokens])
 
   return (
     <main>

b2501c00ac08005c6bbd56184079a8b50d97b309
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-17 22:00:53 +0530

    Adding services

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,37 @@
+import { Card } from "@/components/ui/card"
+import Image from "next/image"
+
 export default function ServicesPage() {
     return (
-      <div>
-        
+      <div className="flex flex-wrap gap-10 items-center justify-center mt-10">
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
+        <Card className="w-[350px] h-[350px] flex flex-col gap-4 p-5">
+          <Image src="/services/phq.png" alt="" width={400} height={400} />
+          <p>what is programming</p>
+        </Card>
       </div>
     )
   }

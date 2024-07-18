8ec276abf896ca073862cdfb63824e257a2b8839
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-18 06:59:31 +0530

    inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
+import { helloWorld } from "@/inngest/function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+    helloWorld
   ],
 });
